b4903c8ceafbaea9483c1cb8af3948136fe8dbc5
Richard Gebhard <info@routes-manager.de>
2021-04-06 15:09:52 +0200

    Bug Benutzerrechte behoben

---
+++ plugins/search/act_building/act_building.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * @package    Com_Act_building
+ * @subpackage Search.XXX_INTERNAL_NAME_XXX
+ * @author     Birgit Gebhard <info@routes-manager.de>
+ * @copyright  2021 Birgit Gebhard
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+require_once JPATH_SITE . '/components/com_act_building/router.php';
+
+use \Joomla\CMS\Factory;
+
+class PlgSearchAct_building extends \Joomla\CMS\Plugin\CMSPlugin
+{
+	/**
+	 * Determine areas searchable by this plugin.
+	 *
+	 * @return  array  An array of search areas.
+	 *
+	 * @since   1.6
+	 */
+	public function onContentSearchAreas()
+	{
+		static $areas = array(
+			'act_building' => 'Act_building'
+		);
+
+		return $areas;
+	}
+
+	/**
+	 * Search XXX_INTERNAL_NAME_XXX.
+	 * The SQL must return the following fields that are used in a common display
+	 * routine: href, title, section, created, text, browsernav.
+	 *
+	 * @param   string  $text      Target search string.
+	 * @param   string  $phrase    Matching option (possible values: exact|any|all).  Default is "any".
+	 * @param   string  $ordering  Ordering option (possible values: newest|oldest|popular|alpha|category).  Default is "newest".
+	 * @param   mixed   $areas     An array if the search it to be restricted to areas or null to search all areas.
+	 *
+	 * @return  array  Search results.
+	 *
+	 * @since   1.6
+	 */
+	public function onContentSearch($text, $phrase = '', $ordering = '', $areas = null)
+	{
+		$db = Factory::getDbo();
+
+		if (is_array($areas))
+		{
+			if (!array_intersect($areas, array_keys($this->onContentSearchAreas())))
+			{
+				return array();
+			}
+		}
+
+		$limit = $this->params->def('search_limit', 50);
+
+		$text = trim($text);
+
+		if ($text == '')
+		{
+			return array();
+		}
+
+		$rows = array();
+
+		
+//Search Buildings.
+if ($limit > 0) {
+    switch ($phrase) {
+        case 'exact':
+            $text = $db->quote('%' . $db->escape($text, true) . '%', false);
+            $wheres2 = array();
+            $wheres2[] = 'a.building LIKE ' . $text;
+            $where = '(' . implode(') OR (', $wheres2) . ')';
+            break;
+
+        case 'all':
+        case 'any':
+        default:
+            $words = explode(' ', $text);
+            $wheres = array();
+
+            foreach ($words as $word) {
+                $word = $db->quote('%' . $db->escape($word, true) . '%', false);
+                $wheres2 = array();
+                $wheres2[] = 'a.building LIKE ' . $word;
+                $wheres[] = implode(' OR ', $wheres2);
+            }
+
+            $where = '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
+            break;
+    }
+
+    switch ($ordering) {
+        default:
+            $order = 'a.id DESC';
+            break;
+    }
+
+    $query = $db->getQuery(true);
+
+    $query
+            ->clear()
+            ->select(
+                    array(
+                        'a.id',
+                        'a.building AS title',
+                        '"" AS created',
+                        'a.building AS text',
+                        '"Building" AS section',
+                        '1 AS browsernav'
+                    )
+            )
+            ->from('#__act_building AS a')
+            
+            ->where('(' . $where . ')')
+            ->group('a.id')
+            ->order($order);
+
+    $db->setQuery($query, 0, $limit);
+    $list = $db->loadObjectList();
+    $limit -= count($list);
+
+    if (isset($list)) {
+        foreach ($list as $key => $item) {
+            $list[$key]->href = JRoute::_('index.php?option=com_act_building&view=building&id=' . $item->id, false, 2);
+        }
+    }
+
+    $rows = array_merge($list, $rows);
+}
+
+		return $rows;
+	}
+}
--- site/views/buildingform/tmpl/default_prozent.php
+++ site/views/buildingform/tmpl/default_prozent.php
@@ -18,7 +18,7 @@ $doc->addScript('node_modules/chart.js/dist/Chart.bundle.min.js');
 $doc->addScript('node_modules/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js');
 //$doc->addScript('components/com_act_building/views/buildingform/tmpl/prozent.js', true, true); 
 $doc->addScript('media/com_routes_planning/js/prozent.js', true, true); 
-$doc->addScript('components/com_act/views/sectorform/tmpl/enternotsend.js'); // Nicht absenden wenn Enter 
+$doc->addScript('media/com_routes_planning/js/enternotsend.js'); // Nicht absenden wenn Enter 
 
 $user    = Factory::getUser();
 $canEdit = Act_buildingHelpersAct_building::canUserEdit($this->item, $user);
